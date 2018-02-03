569384b5dafc68b917960b7a7de48f3132446ef6
Zachary Farmer <zakkjf@gmail.com>
2018-02-03 14:55:52 -0700

    adding comments

--- Problem3/System call/sortstring/sortstring.c
+++ Problem3/System call/sortstring/sortstring.c
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ * Copyright (C) 2018 by Zach Farmer
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for
+ * any misuse of this material.
+ *
+*****************************************************************************/
+/**
+ * @file sortstring.c
+ * @brief Kernel-level system call to sort a provided string
+ *
+ * This is a short example system call designed to sort an input string
+ * and handle user-level errors
+ *
+ * @author Zach Farmer
+ * @date February 3 2018
+ * @version 1.0
+ *
+ */
+
 #include<linux/kernel.h>
 #include<linux/init.h>
 #include<linux/sched.h>
@@ -7,9 +30,10 @@
 #include<linux/errno.h>
 
 SYSCALL_DEFINE3(sort_string, int*, inbuf, int*, outbuf, int, size) {
-
+        //allocate memory for working in kernel space
 	int *kptr = (int*) kmalloc(size * sizeof(int), GFP_KERNEL);
-
+        
+	//check for possible input errors
 	if(kptr==NULL)
 	{
 		printk(KERN_ERR "Kernel Allocation failed\n");
@@ -41,7 +65,7 @@ SYSCALL_DEFINE3(sort_string, int*, inbuf, int*, outbuf, int, size) {
 	printk(KERN_INFO "Buffer size: %d\n", size);
 
 
-
+        //copy memory into kernel space
 	if(copy_from_user(kptr, inbuf, size * sizeof(int))==0)
 	{
 		printk(KERN_INFO "copy_from_user successful\n");
@@ -66,6 +90,7 @@ SYSCALL_DEFINE3(sort_string, int*, inbuf, int*, outbuf, int, size) {
 	
 	printk(KERN_INFO "Finished sort\n");
 
+	//copy memory back out to user space
 	if(copy_to_user(outbuf, kptr, size * sizeof(int))==0)
 	{
 		printk(KERN_INFO "copy_to_user successful\n");
