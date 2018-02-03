6d2899c42db2c08abc5a30f88da8051d9daa37bb
Zachary Farmer <zakkjf@gmail.com>
2018-02-03 14:50:28 -0700

    adding comments

--- Problem2/HW2P2_5013/main.c
+++ Problem2/HW2P2_5013/main.c
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ * Copyright (C) 2018 by Zach Farmer
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for
+ * any misuse of this material.
+ *
+*****************************************************************************/
+/**
+ * @file main.c
+ * @brief Track system calls and library calls with File IO
+ *
+ * This is a short sample program to provide system calls 
+ * and library calls with File IO for tracking
+ *
+ * @author Zach Farmer
+ * @date February 3 2018
+ * @version 1.0
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
